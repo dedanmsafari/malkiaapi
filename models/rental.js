5f4d3f060a6ff104ab425f37dd9e0214c9a75f87
Dedan Msafari <dedan.msafari@gmail.com>
2020-02-07 12:40:08 +0300

    added +50 to rental charges

--- models/rental.js
+++ models/rental.js
@@ -67,7 +67,7 @@ rentalSchema.methods.return = function() {
   this.dateReturned = new Date();
 
   const rentalDays = moment().diff(this.dateOut, 'days');
-  this.rentalFee = rentalDays * this.movie.dailyRentalRate;
+  this.rentalFee = rentalDays * this.movie.dailyRentalRate + 50 ;
 }
 
 const Rental = mongoose.model('Rental', rentalSchema);
